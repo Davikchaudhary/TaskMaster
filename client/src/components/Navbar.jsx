53a5eaabb5727edd266fca328a261ae06397cd68
Davik <Davik.chaudhary@gmail.com>
2024-03-07 12:04:33 +0530

    api

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../assets/images/logo.png';
 
-const Navbar = () => {
+const Navbar = ({username}) => {
   return (
     <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
       <div className="px-3 py-3 lg:px-5 lg:pl-3 flex items-center justify-between">
@@ -25,7 +25,7 @@ const Navbar = () => {
             <div>
               <button type="button" className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
                 <span className="sr-only">Open user menu</span>
-                <span className='text-white text-xl font-medium'>Username</span>
+                <span className='text-white text-xl font-medium'>{username}</span>
               </button>
             </div>
             <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
--- client/src/pages/AppLayout/Home.jsx
+++ client/src/pages/AppLayout/Home.jsx
@@ -3,41 +3,22 @@ import Navbar from '../../components/Navbar';
 import Sidebar from '../../components/Sidebar';
 import Tasks from '../../components/Tasks';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom'; // useNavigate for React Router v6
+import { useNavigate } from 'react-router-dom'; 
 
 const Home = () => {
-  const navigate = useNavigate(); // useNavigate hook for navigation
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userData, setUserData] = useState(null);
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  
+  axios.get("http://localhost:5000/users")
+  .then((response) => {
+    setUsername(response.data)
+  })
+  console.log(username)
 
-  useEffect(() => {
-    const checkLoggedIn = async () => {
-      try {
-        const response = await axios.get("/api/authenticate");
-        if (response.data) {
-          setIsLoggedIn(true);
-          setUserData(response.data);
-        } else {
-          setIsLoggedIn(false);
-          navigate('/login'); // Navigate to the login page
-        }
-      } catch (error) {
-        console.error('Error:', error);
-        alert("An error occurred. Please try again later.");
-      }
-    };
-
-    checkLoggedIn();
-  }, [navigate]);
-
-  if (!isLoggedIn) {
-    // Redirect to login page if not logged in
-    return null; // or you can return <Navigate to="/login" /> if you're using React Router
-  }
   
   return (
     <>
-      <Navbar />
+      <Navbar username={username}/>
       <Sidebar />
       <Tasks />
     </>
