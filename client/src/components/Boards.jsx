48f9af4dde42f8cedac8587b9e950fabeb41ab8f
Davik <Davik.chaudhary@gmail.com>
2024-03-07 18:35:57 +0530

    edit task

--- client/src/components/Boards.jsx
+++ client/src/components/Boards.jsx
@@ -25,8 +25,32 @@ const Boards = () => {
       updatedTasks[columnId].push(newTask); // Push the new task to the array
       return updatedTasks;
     });
-   
   };
+
+  // Inside the editTask function
+const editTask = (columnId, taskIndex, updatedTask) => {
+  setTasks(prevTasks => {
+    const updatedTasks = { ...prevTasks };
+    updatedTasks[columnId][taskIndex] = updatedTask;
+    return updatedTasks;
+  });
+  // Open the modal with the task details for editing
+  setIsModalOpen(true);
+  setColumnId(columnId);
+  setTaskToEdit(updatedTask);
+};
+
+// Add taskToEdit state
+const [taskToEdit, setTaskToEdit] = useState(null);
+
+  const deleteTask = (columnId, taskIndex) => {
+    setTasks(prevTasks => {
+      const updatedTasks = { ...prevTasks };
+      updatedTasks[columnId].splice(taskIndex, 1);
+      return updatedTasks;
+    });
+  };
+
   return (
     <div className="p-4 mt-20 sm:ml-64">
       <div className="flex flex-col">
@@ -42,13 +66,13 @@ const Boards = () => {
           <h3 className="text-right bg-blue-300 p-1 rounded-xl text-black font-semibold">Medium</h3>
           <h3 className="text-right bg-green-300 p-1 rounded-xl text-black font-semibold">Low</h3>
         </div>
-        </div>
+      </div>
 
-        <div className='flex mt-2 flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-10'> 
-        
+      <div className='flex mt-2 flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-10'> 
+        {/* Todo Column */}
         <div className='flex flex-col w-full sm:w-1/4 gap-2'>
           <div className="flex justify-between rounded-xl bg-blue-400 p-4 shadow-lg">
-          <div className="flex h-12 w-12 items-center justify-center rounded-full border border-blue-100 bg-indigo-50">
+            <div className="flex h-12 w-12 items-center justify-center rounded-full border border-blue-100 bg-indigo-50">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
               </svg>
@@ -58,25 +82,26 @@ const Boards = () => {
               <p className="mt-2 text-sm text-gray-600">Last updated</p>
             </div>
             <div className='flex justify-center text-center rounded-lg text-white'>
-            <button onClick={() => handleOpenModal('todo')} className='cursor-pointer rounded-lg bg-slate-600 hover:bg-slate-800 p-2'>Add Tasks</button>
-
+              <button onClick={() => handleOpenModal('todo')} className='cursor-pointer rounded-lg bg-slate-600 hover:bg-slate-800 p-2'>Add Tasks</button>
             </div>
           </div>
-
-          <div className="flex  flex-col w-full p-2 mt-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-blue-100 dark:bg-rose-300">
-            <h5 className="mb-2 text-2xl font-bold text-center text-gray-900 dark:text-black">Task Name</h5>
-            <p className="font-normal text-gray-700 dark:text-gray-900">Task Description</p>
-            <div className='space-x-5 text-end'>
-            <button >Edit Task</button>
-            <button >Delete Task</button>
+          {/* Task List */}
+          {tasks.todo.map((task, index) => (
+            <div key={index} className={`flex flex-col w-full p-2 mt-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-blue-100 dark:bg-rose-300 ${task.priority}`}>
+              <h5 className="mb-2 text-2xl font-bold text-center text-gray-900 dark:text-black">{task.name}</h5>
+              <p className="font-normal text-gray-700 dark:text-gray-900">{task.description}</p>
+              <div className='space-x-5 text-end'>
+                <button onClick={() => editTask('todo', index, { name: 'Updated Task', description: 'Updated Description', priority: 'bg-rose-300' })}>Edit Task</button>
+                <button onClick={() => deleteTask('todo', index)}>Delete Task</button>
+              </div>
             </div>
-          </div>
-          
+          ))}
         </div>
-        
+
+        {/* Backlog Column */}
         <div className='flex flex-col w-full sm:w-1/4 gap-2'>
           <div className="flex justify-between rounded-xl bg-red-400 p-4 shadow-lg">
-          <div className="flex h-12 w-12 items-center justify-center rounded-full border border-red-100 bg-red-50">
+            <div className="flex h-12 w-12 items-center justify-center rounded-full border border-red-100 bg-red-50">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
               </svg>
@@ -86,25 +111,26 @@ const Boards = () => {
               <p className="mt-2 text-sm text-gray-600">Last updated</p>
             </div>
             <div className='flex justify-center text-center rounded-lg text-white'>
-            <button onClick={() => handleOpenModal('backlog')} className='cursor-pointer rounded-lg bg-slate-600 hover:bg-slate-800 p-2'>Add Tasks</button>
-
+              <button onClick={() => handleOpenModal('backlog')} className='cursor-pointer rounded-lg bg-slate-600 hover:bg-slate-800 p-2'>Add Tasks</button>
             </div>
           </div>
-
-          <div className="flex  flex-col w-full p-2 mt-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-blue-100 dark:bg-orange-300">
-            <h5 className="mb-2 text-2xl font-bold text-center text-gray-900 dark:text-black">Task Name</h5>
-            <p className="font-normal text-gray-700 dark:text-gray-900">Task Description</p>
-            <div className='space-x-5 text-end'>
-              <button>Edit Task</button>
-              <button>Delete Task</button>
+          {/* Task List */}
+          {tasks.backlog.map((task, index) => (
+            <div key={index} className={`flex flex-col w-full p-2 mt-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-blue-100 dark:bg-orange-300 ${task.priority}`}>
+              <h5 className="mb-2 text-2xl font-bold text-center text-gray-900 dark:text-black">{task.name}</h5>
+              <p className="font-normal text-gray-700 dark:text-gray-900">{task.description}</p>
+              <div className='space-x-5 text-end'>
+                <button onClick={() => editTask('backlog', index, { name: 'Updated Task', description: 'Updated Description', priority: 'bg-orange-300' })}>Edit Task</button>
+                <button onClick={() => deleteTask('backlog', index)}>Delete Task</button>
+              </div>
             </div>
-          </div>
+          ))}
+        </div>
 
-          </div>
-
-          <div className='flex flex-col w-full sm:w-1/4 gap-2'>
+        {/* In Progress Column */}
+        <div className='flex flex-col w-full sm:w-1/4 gap-2'>
+          {/* In Progress Header */}
           <div className="flex justify-between rounded-xl bg-orange-400 p-4 shadow-lg">
-            
             <div className="flex h-12 w-12 items-center  justify-center rounded-full border border-orange-100 bg-orange-50">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-orange-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
@@ -115,22 +141,25 @@ const Boards = () => {
               <p className="mt-2 text-sm text-gray-600">Last updated</p>
             </div>
             <div className='flex justify-center text-center rounded-lg text-white'>
-            <button onClick={() => handleOpenModal('inProgress')} className='cursor-pointer rounded-lg bg-slate-600 hover:bg-slate-800 p-2'>Add Tasks</button>
-
+              <button onClick={() => handleOpenModal('inProgress')} className='cursor-pointer rounded-lg bg-slate-600 hover:bg-slate-800 p-2'>Add Tasks</button>
             </div>
           </div>
-          
-          <div className="flex  flex-col w-full p-2 mt-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-blue-100 dark:bg-orange-300">
-            <h5 className="mb-2 text-2xl font-bold text-center text-gray-900 dark:text-black">Task Name</h5>
-            <p className="font-normal text-gray-700 dark:text-gray-900">Task Description</p>
-            <div className='space-x-5 text-end'>
-              <button>Edit Task</button>
-              <button>Delete Task</button>
+          {/* Task List */}
+          {tasks.inProgress.map((task, index) => (
+            <div key={index} className={`flex flex-col w-full p-2 mt-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-blue-100 dark:bg-orange-300 ${task.priority}`}>
+              <h5 className="mb-2 text-2xl font-bold text-center text-gray-900 dark:text-black">{task.name}</h5>
+              <p className="font-normal text-gray-700 dark:text-gray-900">{task.description}</p>
+              <div className='space-x-5 text-end'>
+                <button onClick={() => editTask('inProgress', index, { name: 'Updated Task', description: 'Updated Description', priority: 'bg-orange-300' })}>Edit Task</button>
+                <button onClick={() => deleteTask('inProgress', index)}>Delete Task</button>
+              </div>
             </div>
-          </div>
-          </div>
+          ))}
+        </div>
 
-          <div className='flex flex-col w-full sm:w-1/4 gap-2'>
+        {/* Completed Column */}
+        <div className='flex flex-col w-full sm:w-1/4 gap-2'>
+          {/* Completed Header */}
           <div className="flex justify-between rounded-xl bg-green-400 p-4 shadow-lg">
             <div className="flex h-12 w-12 items-center justify-center rounded-full border border-green-100 bg-indigo-50">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-green-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -142,29 +171,25 @@ const Boards = () => {
               <p className="mt-2 text-sm text-gray-600">Last updated</p>
             </div>
             <div className='flex justify-center text-center rounded-lg text-white'>
-            <button onClick={() => handleOpenModal('completed')} className='cursor-pointer rounded-lg bg-slate-600 hover:bg-slate-800 p-2'>Add Tasks</button>
-
+              <button onClick={() => handleOpenModal('completed')} className='cursor-pointer rounded-lg bg-slate-600 hover:bg-slate-800 p-2'>Add Tasks</button>
             </div>
           </div>
-
-            
-          <div className="flex  flex-col w-full p-2 mt-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-blue-100 dark:bg-orange-300">
-            <h5 className="mb-2 text-2xl font-bold text-center text-gray-900 dark:text-black">Task Name</h5>
-            <p className="font-normal text-gray-700 dark:text-gray-900">Task Description</p>
-            <div className='space-x-5 text-end'>
-              <button>Edit Task</button>
-              <button>Delete Task</button>
+          {/* Task List */}
+          {tasks.completed.map((task, index) => (
+            <div key={index} className={`flex flex-col w-full p-2 mt-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-blue-100 dark:bg-orange-300 ${task.priority}`}>
+              <h5 className="mb-2 text-2xl font-bold text-center text-gray-900 dark:text-black">{task.name}</h5>
+              <p className="font-normal text-gray-700 dark:text-gray-900">{task.description}</p>
+              <div className='space-x-5 text-end'>
+                <button onClick={() => editTask('completed', index, { name: 'Updated Task', description: 'Updated Description', priority: 'bg-indigo-300' })}>Edit Task</button>
+                <button onClick={() => deleteTask('completed', index)}>Delete Task</button>
+              </div>
             </div>
-          </div>
-          
-          </div>
-          </div>
-          {isModalOpen && <CreateTasks onClose={() => setIsModalOpen(false)} columnId="todo" addTask={addTask} />}
+          ))}
+        </div>
+      </div>
 
-         </div>
-     
-     
-      
+      {isModalOpen && <CreateTasks onClose={() => setIsModalOpen(false)} columnId={columnId} addTask={addTask} taskToEdit={taskToEdit} />}
+    </div>
   );
 };
 
--- client/src/components/CreateTasks.jsx
+++ client/src/components/CreateTasks.jsx
@@ -1,33 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const CreateTasks = ({ onClose, columnId, addTask }) => {
+const CreateTasks = ({ onClose, columnId, addTask, taskToEdit }) => {
+  // Initialize form state with taskToEdit details if provided
+  const [formData, setFormData] = useState({
+    name: taskToEdit ? taskToEdit.name : '',
+    priority: taskToEdit ? taskToEdit.priority : '',
+    description: taskToEdit ? taskToEdit.description : ''
+  });
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const taskName = event.target.name.value;
-        const taskPriority = event.target.category.value;
-        const taskDescription = event.target.description.value;
-        
-        const newTask = {
-          name: taskName,
-          priority: taskPriority,
-          description: taskDescription
-        };
-        
-        addTask(columnId, newTask); // Add task to the specific column
-        
-        onClose();
-      };
-      
+  // Handle changes to form fields
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData(prevData => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
 
+  // Handle form submission
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newTask = {
+      name: formData.name,
+      priority: formData.priority,
+      description: formData.description
+    };
+    addTask(columnId, newTask);
+    onClose();
+  };
 
   return (
-    <div id="crud-modal" className={` fixed top-0 left-0 z-50 flex justify-center items-center w-full h-full bg-black bg-opacity-50`}>
+    <div id="crud-modal" className={`fixed top-0 left-0 z-50 flex justify-center items-center w-full h-full bg-black bg-opacity-50`}>
       <div className="relative w-full max-w-md mx-auto bg-gray-700 rounded-lg shadow-lg">
         <div className="flex items-center justify-between p-4 border-b">
           <h3 className="text-lg font-semibold text-white">Create New Task</h3>
           <button
-            onClick={onClose} // Use onClose directly
+            onClick={onClose}
             className="text-gray-400 hover:text-gray-700 focus:outline-none"
           >
             <svg
@@ -46,47 +54,63 @@ const CreateTasks = ({ onClose, columnId, addTask }) => {
           </button>
         </div>
         <form onSubmit={handleSubmit} className="p-4">
-            <div className="grid grid-cols-2 gap-4">
-              <div>
-                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  Task Name
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  className="bg-gray-50 border mt-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                  placeholder="Type Board name..."
-                  required
-                />
-              </div>
-              <div className="col-span-2 sm:col-span-1">
-                        <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Priority</label>
-                        <select id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                            <option defaultValue="">Select Priority</option>
-                            <option value="I">Immediate</option>
-                            <option value="H">High</option>
-                            <option value="M">Medium</option>
-                            <option value="L">Low</option>
-                        </select>
-                    </div>
-              <div className="col-span-2">
-                <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Task Description</label>
-                <textarea id="description" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write task description here"></textarea>                    
+          <div className="grid grid-cols-2 gap-4">
+            <div>
+              <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Task Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name} // Add value attribute to bind input value to state
+                onChange={handleChange} // Add onChange event to update state on input change
+                className="bg-gray-50 border mt-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Type Board name..."
+                required
+              />
             </div>
-            </div>
-            <div className="mt-4">
-              <button
-                type="submit"
-                className="w-full px-4 py-2 bg-blue-400 border border-transparent rounded-md shadow-sm text-base font-medium text-black hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
+            <div className="col-span-2 sm:col-span-1">
+              <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Priority</label>
+              <select
+                id="category"
+                name="priority"
+                value={formData.priority} // Add value attribute to bind select value to state
+                onChange={handleChange} // Add onChange event to update state on select change
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
               >
-                Add new Task
-              </button>
+                <option value="">Select Priority</option>
+                <option value="bg-rose-300">Immediate</option>
+                <option value="bg-orange-300">High</option>
+                <option value="bg-blue-300">Medium</option>
+                <option value="bg-green-300">Low</option>
+              </select>
             </div>
-          </form>
-        </div>
+            <div className="col-span-2">
+              <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Task Description</label>
+              <textarea
+                id="description"
+                name="description"
+                value={formData.description} // Add value attribute to bind textarea value to state
+                onChange={handleChange} // Add onChange event to update state on textarea change
+                rows="4"
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Write task description here"
+              ></textarea>
+            </div>
+          </div>
+          <div className="mt-4">
+            <button
+              type="submit"
+              className="w-full px-4 py-2 bg-blue-400 border border-transparent rounded-md shadow-sm text-base font-medium text-black hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
+            >
+              Add new Task
+            </button>
+          </div>
+        </form>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-export default CreateTasks
+export default CreateTasks;
