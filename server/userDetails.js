0c281266ac8f74627fcf16a8344237aa92c9a9ee
unknown <garg31916@gmail.com>
2024-03-07 18:38:13 +0530

    change schema

--- server/userDetails.js
+++ server/userDetails.js
@@ -5,6 +5,7 @@ const UserDetailsSchema = new mongoose.Schema(
         uname: String,
         email: {type: String, unique: true},
         password: String,
+        boards: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Board' }],
     },
     {
         collection: "UserInfo",
