fffc25aab025c9ac8b69d3aade565a4fe7f106a8
Davik <Davik.chaudhary@gmail.com>
2024-03-07 11:10:26 +0530

    sync

--- client/src/pages/AppLayout/Home.jsx
+++ client/src/pages/AppLayout/Home.jsx
@@ -6,20 +6,31 @@ import axios from 'axios';
 
 const Home = () => {
 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
 
   useEffect(()=>{
-    axios.post("http://localhost/5000/login-user"),{
+    axios.get("http://localhost/5000/login-user"),{
       uname,
       email,
     }
     .then((response)=>{
       console.log(response)
       if(response.data.status==='ok'){
-        
+        setIsLoggedIn(true);
+        setUsername(response.data.uname);
+        setEmail(response.data.email);
+      }
+      else {
+        setIsLoggedIn(false);
+        NavigationPreloadManager('/login')
       }
     })
+    .catch((error) => {
+      console.error('Error:', error);
+      alert("An error occurred. Please try again later.");
+  });
 
   },[])
   
