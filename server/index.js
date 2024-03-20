87516044af2c1b4fa123f8b3a101699e9d92e451
unknown <garg31916@gmail.com>
2024-03-20 15:46:51 +0530

    getallusers

--- server/index.js
+++ server/index.js
@@ -237,3 +237,14 @@ app.post('/board/:boardId/addBoardToUsers', async (req, res) => {
 
 
 //ok
+
+//get all users
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
