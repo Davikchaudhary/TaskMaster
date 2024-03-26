321a9190f33d9f5cd8957b4c6fd37c631964eafb
Davik <Davik.chaudhary@gmail.com>
2024-03-26 22:05:52 +0530

    task name

--- client/src/components/CreateTasks.jsx
+++ client/src/components/CreateTasks.jsx
@@ -72,7 +72,7 @@ const CreateTasks = ({ onClose, columnId, addTask, taskToEdit }) => {
                 value={formData.title}
                 onChange={handleChange}
                 className="bg-gray-50 border mt-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                placeholder="Type Board name..."
+                placeholder="Type Task name..."
                 required
               />
             </div>
