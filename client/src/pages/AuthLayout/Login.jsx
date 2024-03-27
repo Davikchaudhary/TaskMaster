34b89253433c9c1b9f126a0a29fedc6fb9ad4bd4
unknown <garg31916@gmail.com>
2024-03-27 10:55:22 +0530

    final

--- client/src/pages/AuthLayout/Login.jsx
+++ client/src/pages/AuthLayout/Login.jsx
@@ -73,7 +73,7 @@ const Login = () => {
                   id="email"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="name@domain.com"
-                  minLength={8}
+                  
                   value={email}
                   onChange={handleEmailChange}
                   required
@@ -93,6 +93,7 @@ const Login = () => {
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   value={password}
+                  minLength={8}
                   onChange={handlePasswordChange}
                   required
                 />
